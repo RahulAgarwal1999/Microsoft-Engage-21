aee822790133bd1ffef058f6f0730b4ac4baea08
RahulAgarwal31101999 <rahul.agarwal31101999@gmail.com>
2021-11-22 20:04:40 +0530

    error 404 page added

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -26,3 +26,4 @@ urlpatterns = [
 ]
 # urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = "mysite.views.page_not_found_view"
